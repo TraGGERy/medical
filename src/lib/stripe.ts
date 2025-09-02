07a3c13d5f595c095cffcce3367e474224f5f939
TraGGery <traggerozzyboune@gmail.com>
2025-09-02 00:09:00 -0700

    11

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY is not set in environment variables');
 }
-
+// Create a Stripe client
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2025-07-30.basil',
   typescript: true,
